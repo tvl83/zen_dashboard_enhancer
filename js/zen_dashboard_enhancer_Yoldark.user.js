452ee19d56dc8ea0d11b595e6626ec470c84922e
Yoldark34 <yoldark@gmail.com>
2014-10-07 15:50:46 +0200

    Use financial as external lib

--- js/zen_dashboard_enhancer_Yoldark.user.js
+++ js/zen_dashboard_enhancer_Yoldark.user.js
@@ -86,125 +86,7 @@ if (GM_getValue('CLOCK_DIFF', null) === null) {
     GM_setValue('CLOCK_DIFF', CLOCK_DIFF);
 }
 this.$ = this.jQuery = jQuery.noConflict(true);
-var financialTrackingChart;
 
-function requestFinancialData(callback) {
-    $.ajax({
-        url: AJAX_RETRIEVE_FINANCIAL_DATA,
-        data: {
-            iDisplayLength: 0
-        },
-        beforeSend: function() {
-            addConfigurationElement('Populate financial chart', 'financial-chart-population');
-        },
-        success: function (response) {
-            var aaData = response.aaData;
-            var dayIndex = 0;
-            var debitIndex = 3;
-            var creditIndex = 2;
-            var aData = null;
-            var debits = [];
-            var credits = [];
-            var income = [];
-            var debitsGrouped = [];
-            var creditsGrouped = [];
-            var days = [];
-            var currentDate = null;
-            var daysRange = 0;
-            var debitAmount;
-            var creditAmount;
-            var currentIncome;
-            var dayStart = 0;
-            var i;
-
-            //Remove the previous series
-            for (i = financialTrackingChart.series.length; i > 0; i--) {
-                financialTrackingChart.series[(i - 1)].remove();
-            }
-            // avoid series changing color
-            financialTrackingChart.colorCounter = 0;
-
-            //Initialize dates to groups debits and credits by days
-            for (i = 0; i < aaData.length; i++) {
-                daysRange++;
-                aData = aaData[i];
-                //Parse to yyyy/MM/dd to properly sort
-                currentDate = Date.parse(aData[dayIndex]).toString('yyyy/MM/dd');
-                if ($.inArray(currentDate, days) === -1) {
-                    days.push(currentDate);
-                }
-                debitsGrouped[currentDate] = 0;
-                creditsGrouped[currentDate] = 0;
-            }
-
-            //Sort all the days
-            days.sort();
-
-            //Fill the debits and credits grouped by days
-            for (i = 0; i < aaData.length; i++) {
-                aData = aaData[i];
-                currentDate = Date.parse(aData[dayIndex]).toString('yyyy/MM/dd');
-                type = aData[1].split('>') [1].split('<') [0];
-                if (type === 'Payout' || type === 'Maintenance Fee') {
-                    if (aData[debitIndex].length) {
-                        debitAmount = aData[debitIndex]
-                            .split('>') [1]
-                            .split('<') [0]
-                            .split(' ') [0];
-                        debitsGrouped[currentDate] -= parseFloat(debitAmount);
-                    } else if (aData[creditIndex].length) {
-                        creditAmount = aData[creditIndex]
-                            .split('>') [1]
-                            .split('<') [0]
-                            .split(' ') [0];
-                        creditsGrouped[currentDate] += parseFloat(creditAmount);
-                    }
-                }
-            }
-
-            if (days.length > GM_getValue('FINANCIAL_DISPLAYED_DAYS_QUANTITY')) {
-                dayStart = days.length - GM_getValue('FINANCIAL_DISPLAYED_DAYS_QUANTITY');
-            }
-
-            //Display only the last xxx days
-            for (i = dayStart; i < days.length; i++) {
-                //Need to convert big float value due to error calculation into correct
-                //8 digit length after comma value.
-                debits.push(parseFloat(-debitsGrouped[days[i]].toFixed(8)));
-                credits.push(parseFloat(creditsGrouped[days[i]].toFixed(8)));
-                currentIncome = creditsGrouped[days[i]] - debitsGrouped[days[i]];
-                income.push(parseFloat(currentIncome.toFixed(8)));
-            }
-
-            //Remove unused days
-            days.splice(0, dayStart);
-
-            //Add the series to the chart
-            financialTrackingChart.colorCounter = 10;
-            financialTrackingChart.addSeries({
-                data: income,
-                name: 'Income'
-            });
-            financialTrackingChart.colorCounter = 5;
-            financialTrackingChart.addSeries({
-                data: debits,
-                name: 'Debits'
-            });
-            financialTrackingChart.colorCounter = 2;
-            financialTrackingChart.addSeries({
-                data: credits,
-                name: 'Credits'
-            });
-
-            //Update the xAxis according to the days
-            financialTrackingChart.xAxis[0].setCategories(days);
-            validateConfigurationElement('financial-chart-population');
-            if (callback) {
-                callback();
-            }
-        }
-    });
-}
 function displayTopDashboard(container) {
     addConfigurationElement('Display top dashboard data', 'top-dashboard-data');
     var bitcoin = parseFloat($('.balance-value').html());
@@ -256,55 +138,6 @@ function displayTopDashboard(container) {
         }
     }
     validateConfigurationElement('top-dashboard-data');
-}
-
-function initializeFinancialChart(container, callback) {
-    addConfigurationElement('Create financial chart', 'financial-chart-creation');
-    if ($(container).length) {
-        $(container).prepend('<div id="financialTrackingContainer" style="min-width: 310px; height: 400px; margin: 0 auto"></div>');
-        financialTrackingChart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'financialTrackingContainer',
-                defaultSeriesType: 'spline',
-                events: {
-                    load: requestFinancialData(callback)
-                }
-            },
-            title: {
-                text: 'Payouts tracking',
-                x: -20 //center
-            },
-            xAxis: {
-                title: {
-                    text: ' Day'
-                },
-                type: 'category',
-                labels: {
-                    rotation: -75
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'BTC'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' BTC'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            exporting: {
-                csv: {
-                    dateFormat: '%Y-%m-%d'
-                }
-            }
-        });
-        validateConfigurationElement('financial-chart-creation');
-    }
 }
 
 function checkShitMode(callback) {
@@ -597,7 +430,7 @@ function configurationChecker() {
     displayLogMessage('Greasemonkey config checking error', MESSAGE_TYPE_ERROR);
 
     var callback = function() {
-        financialTrackingChart = null;
+        console.log('test');
         mockDiv.empty();
         displayTopDashboard(mockDiv);
         mockDiv.empty();
@@ -628,7 +461,7 @@ function configurationChecker() {
             });
         });
     };
-    initializeFinancialChart(mockDiv, callback);
+    new Financial(mockDiv, AJAX_RETRIEVE_FINANCIAL_DATA, callback);
 }
 
 function requestRoiData(callback) {
@@ -734,7 +567,8 @@ function main() {
 
         if (page === 'Balance') {
             if (GM_getValue('ACTIVATE_FINANCIAL')) {
-                initializeFinancialChart($('.mb10 .panel-body'));
+                new Financial($('.mb10 .panel-body'), AJAX_RETRIEVE_FINANCIAL_DATA);
+                //initializeFinancialChart($('.mb10 .panel-body'));
             }
         } else if (page === 'Profile') {
             initializeConfigPanel($('div.col-lg-6.col-md-6.col-sm-6.col-xs-12 .panel:first'));
