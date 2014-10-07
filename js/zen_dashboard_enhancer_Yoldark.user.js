ad5a7a4c8244e31d56852c860492b53da77ce636
Yoldark34 <yoldark@gmail.com>
2014-10-07 11:35:19 +0200

    Change highcharts library in external to avoid encoding problems

--- js/zen_dashboard_enhancer_Yoldark.user.js
+++ js/zen_dashboard_enhancer_Yoldark.user.js
@@ -12,7 +12,7 @@
 // @grant       GM_getResourceText
 // @grant       GM_addStyle
 // @require     https://rawgit.com/Yoldark34/zen_dashboard_enhancer/raw/master/js/libraries/jquery-1.11.1.min.js
-// @require     https://rawgit.com/Yoldark34//zen_dashboard_enhancer/raw/master/js/libraries/highcharts.js
+// @require     http://code.highcharts.com/highcharts.js
 // @require     https://rawgit.com/Yoldark34/Yoldark34/zen_dashboard_enhancer/raw/master/js/libraries/exporting.js
 // @require     https://rawgit.com/Yoldark34/zen_dashboard_enhancer/raw/master/js/libraries/export-csv.js
 // @require     https://raw.githubusercontent.com/Yoldark34/zen_dashboard_enhancer/raw/master/js/libraries/date.js
@@ -630,6 +630,67 @@ function configurationChecker() {
     initializeFinancialChart(mockDiv, callback);
 }
 
+function requestRoiData(callback) {
+
+}
+
+function initializeRoiChart(container, callback) {
+    $(container).prepend($('<div>', {id : 'yoldark-roi-chart', class : 'row'}));
+    financialTrackingChart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'yoldark-roi-chart',
+                defaultSeriesType: 'spline',
+                events: {
+                    load: requestRoiData(callback)
+                }
+            },
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
+}
+
 function main() {
     if (GM_getValue("MODAL_MESSAGE_SUCCESS_QUEUE", false)) {
         initializeModalDialog('Yoldark dashboard enhancer config checking result',
@@ -676,6 +737,8 @@ function main() {
             }
         } else if (page === 'Profile') {
             initializeConfigPanel($('div.col-lg-6.col-md-6.col-sm-6.col-xs-12 .panel:first'));
+        } else if (page === 'Miners') {
+            initializeRoiChart($('div#content div.content-wrapper div.outlet'));
         }
         checkShitMode();
     }
