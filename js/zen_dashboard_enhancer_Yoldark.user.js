55d423577c5b66b020ee3aeb6a81b7425d8ad9bd
Thomas Le <tvle83@gmail.com>
2014-10-25 17:44:03 +0300

    Figuring stuff out

--- js/zen_dashboard_enhancer_Yoldark.user.js
+++ js/zen_dashboard_enhancer_Yoldark.user.js
@@ -507,7 +507,6 @@ function main() {
             var btcValue = (dollars / bitcoin).toString();
             btcValue = btcValue.substring(0, 6);
 
-
             var priceColumn = $('td:nth-child(3)');
             var unitPriceColumn = $('td:nth-child(4)');
 
@@ -516,9 +515,10 @@ function main() {
             for(var i = 0; i< 100; i++){
                 priceTemp = priceColumn[i].innerText.replace('$', '');
                 priceTemp = priceTemp / btcValue;
-                unitTemp = parseFloat(unitPriceColumn[i].innerText) / btcValue;
+                unitTemp = unitPriceColumn[i].innerText / btcValue;
                 priceColumn[i].innerText = priceColumn[i].innerText + " ( BTC: " + priceTemp.toFixed(8) + " ) ";
                 unitPriceColumn[i].innerText = unitPriceColumn[i].innerText + " ( BTC: " + unitTemp.toFixed(8) + " ) ";
+                console.log ( "index: " + i);
             }
         }
         checkShitMode();
